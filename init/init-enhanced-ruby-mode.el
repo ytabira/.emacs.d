adf8fe2554cd6753b0951ebaf5056dbd6fc3dbef
Yasuo Tabira <ytabira@gmail.com>
2015-10-28 20:41:36 +0900

    Berksfileでruby-modeが起動するように追加。

--- init/init-enhanced-ruby-mode.el
+++ init/init-enhanced-ruby-mode.el
@@ -2,3 +2,4 @@
 (add-to-list 'auto-mode-alist '("\\.rb$" . enh-ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.jpbuilder$" . enh-ruby-mode))
 (add-to-list 'interpreter-mode-alist '("ruby" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("Berksfile$" . enh-ruby-mode))
